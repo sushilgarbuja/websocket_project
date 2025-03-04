438a25102572cb2ec71861d8ba966f03b9a74e13
sushil <sushilgarbuja82335@gmail.com>
2025-03-04 22:51:53 +0545

    learn basic

--- server.ts
+++ server.ts
@@ -2,14 +2,35 @@ import { env } from 'process';
 import app from './src/app';
 import { envConfig } from './src/config/config';
 import connection from './src/config/db';
+import { Server } from 'socket.io';
 
+
+//data receive garda on
+//data send garda emit
+//request socket
+//api event
+//req.body=data
+//io le sbailai
+//socket le conected lai matra
 
 function startServer() {
     connection();
     const port = envConfig.port||4000
-    app.listen(port, () => {
+    
+   const server= app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
-    });
+    })
+    const io = new Server(server)
+    io.on("connection",(socket)=>{
+        socket.on("haha",(data)=>{
+            console.log(data) 
+            io.emit("response",{
+            message:"sushil"
+        })
+    })
+        console.log("socket connected")
+    })
+
 }
 
 startServer();
